7bf6b720f5159506d7a73f82b42349d58dd79583
savas <savasusame@gmail.com>
2023-04-29 15:31:00 +0300

    fix PWA

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,10 +6,6 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
         <meta name="description" content="description of your project" />
         <meta name="theme-color" content="#000" />
         <link rel="manifest" href="/manifest.json" />
